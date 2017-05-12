7126bf8319519e60a42c11f804c94ec2a0c77118
= <=>
2017-05-12 15:53:11 +0800

    modify JVMTestHandler

--- JVMTestHandler/src/JVMTestHandler.java
+++ JVMTestHandler/src/JVMTestHandler.java
@@ -3,8 +3,8 @@ public class JVMTestHandler {
 
     public static void main(String args[])
     {
-        //new JVMTestHandler().doTest();
-        new JVMTestHandler().infinitelyRecursiveMethod(1);
+        new JVMTestHandler().doTest();
+        //new JVMTestHandler().infinitelyRecursiveMethod(1);
     }
     
     public void doTest()
